ac7ae9e0163b5135df187b036407cf598cb63a3e
Ryan Bishop <rtbishop@ucsc.edu>
2018-05-15 18:23:33 -0700

    removed flash message from index

--- controllers/default.py
+++ controllers/default.py
@@ -17,8 +17,7 @@ def index():
     if you need a simple wiki simply replace the two lines below with:
     return auth.wiki()
     """
-    response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+    return dict()
 
 
 def user():
